f6d0aad713c5c09c59573f3b4d1f1c22a25c5220
Andrew Collins <collina2@uci.edu>
2023-12-06 11:45:03 -0800

    next batch

--- examples/evaluate.py
+++ examples/evaluate.py
@@ -28,10 +28,6 @@ def load_model(model_path, env=None, position=None, device=None):
         import torch
         agent = torch.load(model_path, map_location=device)
         agent.set_device(device)
-    elif os.path.isdir(model_path):  # CFR model
-        from rlcard.agents import CFRAgent
-        agent = CFRAgent(env, model_path)
-        agent.load()
     elif model_path == 'random':  # Random model
         from rlcard.agents import RandomAgent
         agent = RandomAgent(num_actions=env.num_actions)
@@ -72,14 +68,7 @@ if __name__ == '__main__':
         type=str,
         default='uno',
         choices=[
-            'blackjack',
-            'leduc-holdem',
-            'limit-holdem',
-            'doudizhu',
-            'mahjong',
-            'no-limit-holdem',
             'uno',
-            'gin-rummy',
         ],
     )
     parser.add_argument(
